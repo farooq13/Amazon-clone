e3bcc78b9367f7634c5777b2bd1db1946a3cf1d3
farooq13 <farukidris19@gmail.com>
2024-12-02 18:03:20 +0100

    add automated testing

---
+++ amzon-clone/test/moneyTest.js
@@ -0,0 +1,31 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+
+console.log('test suite: formatCurrency');
+
+
+console.log('converts cents into dollars');
+
+if (formatCurrency(2095) === '20.95') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+
+console.log('works with 0');
+
+if (formatCurrency(0) === '0.00') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+
+console.log('rounds to the nearest cents');
+
+if (formatCurrency(2000.5) === '20.01') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
